c6ad0280e11f188cafd1e55498a1b4031ec3b2d0
isliuxiaodong <773829073@qq.com>
2018-04-03 14:22:58 +0800

    购物车删除修改

--- ttsx/apps/tt_cart/views.py
+++ ttsx/apps/tt_cart/views.py
@@ -87,5 +87,66 @@ def index(request):
         'sku_list':sku_list,
 
     }
-
     return render(request,'cart.html',context)
+
+
+def edit(request):
+    if request.method!='POST':
+        return Http404()
+    dict = request.POST
+    sku_id = dict.get('sku_id', 0)
+    count = dict.get('count', 0)
+
+    # 验证数据的有效性
+    # 判断商品是否存在
+    if GoodsSKU.objects.filter(pk=sku_id).count() <= 0:
+        return JsonResponse({'status': 2})
+    # 判断数量是一个有效的数字
+    try:
+        count = int(count)
+    except:
+        return JsonResponse({'status': 3})
+    # 判断数量大于0并小于等于5
+    if count <= 0:
+        count = 1
+    elif count >= 5:
+        count = 5
+
+    response = JsonResponse({'status': 1})
+    # 改写购物车中的数量
+    if request.user.is_authenticated():
+        # 如果已登录则操作redis
+        redis_client = get_redis_connection()
+        redis_client.hset('cart%d' % request.user.id, sku_id, count)
+    else:
+        # 如果未登录则操作cookie
+        cart_str = request.COOKIES.get('cart')
+        if cart_str:
+            cart_dict = json.loads(cart_str)
+            # 改写数量
+            cart_dict[sku_id] = count
+            # 将字典转成字符串，用于cookie保存
+            cart_str = json.dumps(cart_dict)
+            # 写cookie，保存购物车信息
+            response.set_cookie('cart', cart_str, expires=60 * 60 * 24 * 14)
+
+    return response
+
+def delete(request):
+    if request.method != 'POST':
+        return Http404()
+    sku_id = request.POST.get('sku_id')
+
+    response = JsonResponse({'status': 1})
+
+    if request.user.is_authenticated():
+        redis_client = get_redis_connection()
+        redis_client.hdel('cart%d'% request.user.id, sku_id)
+    else:
+        cart_str = request.COOKIES.get('cart')
+        if cart_str:
+            cart_dict = json.loads(cart_str)
+            cart_dict.pop(sku_id)
+            cart_str = json.dumps(cart_dict)
+            response.set_cookie('cart', cart_str, expires=60 * 60 * 24 * 14)
+    return response
